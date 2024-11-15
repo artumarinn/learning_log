d385827361957a6775524664a2368f4500485a7b
artumarinn <artumarin.09@gmail.com>
2024-11-14 21:07:06 -0300

    view: topics

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,4 +7,7 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Pagina de inicio
     path('', views.index, name='index'),
+    # Pagina que muestra todos los temas
+    path('topics/', views.topics, name='topics')
 ]
+
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render # Muestras las respuesta de los datos proporcionados por las vistas
+from .models import Topic
 
 def index(request):
     """La pagina de inicio para Learning Log."""
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Muestra todos los temas."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
