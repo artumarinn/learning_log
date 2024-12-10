2e2c05349b3a14dcec5e52372659227f50641136
artumarinn <artumarin.09@gmail.com>
2024-12-10 13:26:31 -0300

    view: new_topic

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render # Muestras las respuesta de los datos proporcionados por las vistas
+from django.shortcuts import render, redirect # Muestras las respuesta de los datos proporcionados por las vistas
 from .models import Topic
+from .forms import TopicForm
 
 def index(request):
     """La pagina de inicio para Learning Log."""
@@ -18,3 +19,18 @@ def topic(request, topic_id):
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
+def new_topic(request):
+    """Añade un tema nuevo"""
+    if request.method != 'POST':
+        # No se ha enviado datos; crea un formulario en blanco.
+        form = TopicForm()
+    else:
+        # Datos POST enviados; procesa datos.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs/new_topic.html', context)
+    
+    # Muestra un formulario en blanco o no valido.
+    context = {'form': form} 
+    return render(request, 'learning_logs/new_topic.html', context)
